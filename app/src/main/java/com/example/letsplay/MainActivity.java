067f8da7117840c8373d48b8e260de9817c7f9e5
Vishal Kothari <vishal.kk@somaiya.edu>
2020-07-01 16:49:17 +0530

    added the settting page complete

--- app/src/main/java/com/example/letsplay/MainActivity.java
+++ app/src/main/java/com/example/letsplay/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.letsplay;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.design.widget.TabItem;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
     Toolbar mToolbar;
@@ -14,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     TabItem libraryTabItem;
     TabItem aartiTabItem;
     ViewPager mViewPager;
+    ImageButton setting;
 
 
 
@@ -22,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mToolbar = findViewById(R.id.toolbar);
+        setting = findViewById(R.id.setting);
         setSupportActionBar(mToolbar);
         getSupportActionBar().setTitle(getString(R.string.app_name));
 
@@ -33,7 +39,15 @@ public class MainActivity extends AppCompatActivity {
 
         mPagerAdapter = new PagerAdapter(getSupportFragmentManager(),mTabLayout.getTabCount()); // call the pager class
         mViewPager.setAdapter(mPagerAdapter); // set adapter for pager
+        setting.setOnClickListener(new View.OnClickListener() {
 
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(MainActivity.this,Setting.class);
+                startActivity(myIntent);
+            }
+
+        });
         // do something when the tab is selected
         mTabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
 
